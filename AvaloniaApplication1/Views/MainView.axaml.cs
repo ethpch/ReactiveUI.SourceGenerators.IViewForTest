9b6197ba778fe80b83193504a250b26b1f99eb12
ethpch <ethpch@outlook.com>
2024-09-26 13:55:55 +0800

    添加项目文件。

---
+++ AvaloniaApplication1/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls;
+
+namespace AvaloniaApplication1.Views;
+
+[ReactiveUI.SourceGenerators.IViewFor<ViewModels.MainViewModel>]
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
